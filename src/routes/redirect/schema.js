3eb08c6bc580126796f12091b2c7c073fd173454
Frazer Smith <frazerry@outlook.com>
2021-01-27 09:51:06 +0000

    fix(routes/redirect): add required properties

--- src/routes/redirect/schema.js
+++ src/routes/redirect/schema.js
@@ -15,7 +15,8 @@ const redirectGetSchema = {
 	params: S.object()
 		// Longest STU3 FHIR resource name is 'ImmunizationRecommendation' at 26 chars
 		.prop("resource", S.string().pattern("^\\w{1,26}$"))
-		.prop("id", S.string().pattern("^[\\d-]+$")),
+		.prop("id", S.string().pattern("^[\\d-]+$"))
+		.required(["resource"]),
 
 	// Querystring search parameters from https://www.hl7.org/fhir/STU3/search.html
 	query: S.object()
@@ -29,7 +30,7 @@ const redirectGetSchema = {
 					.pattern(
 						"^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\\d{4}-\\d{2}-\\d{2}(?:T\\d{2}\\:\\d{2}\\:\\d{2}|)$"
 					),
-				S.string().description("string").pattern('^[^<>"]+$'),
+				S.string().description("string").pattern("^[^<>\"']+$"),
 				S.string().description("uri").format("uri"),
 			]),
 		})

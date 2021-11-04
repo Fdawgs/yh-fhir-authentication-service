bc762cc438ba181544fd791e57df25a3ebb9ecb1
Frazer Smith <frazer.dev@outlook.com>
2021-11-04 10:38:09 +0000

    test: ignore else routes in coverage

--- src/routes/redirect/index.js
+++ src/routes/redirect/index.js
@@ -26,12 +26,13 @@ async function route(server, options) {
 		.register(bearer, {
 			addHook: false,
 			keys: options.bearerTokenAuthKeys,
-			/* istanbul ignore next */
-			errorResponse: (err) => ({
-				statusCode: 401,
-				error: "Unauthorized",
-				message: err.message,
-			}),
+			errorResponse:
+				/* istanbul ignore next */
+				(err) => ({
+					statusCode: 401,
+					error: "Unauthorized",
+					message: err.message,
+				}),
 		})
 		.register(replyFrom, {
 			base: options.redirectUrl,

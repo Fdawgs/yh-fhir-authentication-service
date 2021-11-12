0e1e5fe767ce75156fe221023ec2786ea8be8870
Frazer Smith <frazer.dev@outlook.com>
2021-11-12 13:27:28 +0000

    refactor: use custom error handler; link errors to requests in logs

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ async function plugin(server, config) {
 
 	server
 		// Utility functions and error handlers
-		.register(sensible)
+		.register(sensible, { errorHandler: false })
 
 		// Re-usable schemas
 		.register(sharedSchemas)
@@ -97,7 +97,21 @@ async function plugin(server, config) {
 					dirNameRoutePrefix: false,
 					options: config,
 				});
-		});
+		})
+
+		// Errors thrown by routes and plugins are caught here
+		.setErrorHandler(
+			// eslint-disable-next-line promise/prefer-await-to-callbacks
+			(err, req, res) => {
+				if (res.statusCode >= 500) {
+					req.log.error({ req, res, err }, err && err.message);
+					res.internalServerError();
+				} else {
+					req.log.info({ req, res, err }, err && err.message);
+					res.send(err);
+				}
+			}
+		);
 }
 
 module.exports = fp(plugin, { fastify: "3.x", name: "server" });

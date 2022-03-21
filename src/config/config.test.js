d2b905b0f01529116f98fd00c4a598a995fe287a
Frazer Smith <frazer.dev@outlook.com>
2022-03-21 11:04:29 +0000

    refactor(server): use new hook config option for rate-limit plugin (#546)

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -121,6 +121,7 @@ describe("Configuration", () => {
 		expect(config.rateLimit).toEqual({
 			allowList: JSON.parse(RATE_LIMIT_EXCLUDED_ARRAY),
 			continueExceeding: true,
+			hook: "onSend",
 			max: 1000,
 			timeWindow: 60000,
 		});
@@ -226,6 +227,7 @@ describe("Configuration", () => {
 		expect(config.rateLimit).toEqual({
 			allowList: JSON.parse(RATE_LIMIT_EXCLUDED_ARRAY),
 			continueExceeding: true,
+			hook: "onSend",
 			max: RATE_LIMIT_MAX_CONNECTIONS_PER_MIN,
 			timeWindow: 60000,
 		});

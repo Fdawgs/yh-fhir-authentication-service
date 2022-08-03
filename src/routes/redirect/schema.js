c13efbb991c504628a12f61b17085c12b29925a6
Frazer Smith <frazer.dev@outlook.com>
2022-08-03 11:32:24 +0100

    fix(routes): invalid openapi shared schema definitions (#730)

--- src/routes/redirect/schema.js
+++ src/routes/redirect/schema.js
@@ -64,16 +64,16 @@ const redirectGetSchema = {
 		})
 		.additionalProperties(false),
 	response: {
-		401: S.ref("responses#/definitions/unauthorized").description(
+		401: S.ref("responses#/properties/unauthorized").description(
 			"Unauthorized"
 		),
-		406: S.ref("responses#/definitions/notAcceptable").description(
+		406: S.ref("responses#/properties/notAcceptable").description(
 			"Not Acceptable"
 		),
-		429: S.ref("responses#/definitions/tooManyRequests").description(
+		429: S.ref("responses#/properties/tooManyRequests").description(
 			"Too Many Requests"
 		),
-		503: S.ref("responses#/definitions/serviceUnavailable").description(
+		503: S.ref("responses#/properties/serviceUnavailable").description(
 			"Service Unavailable"
 		),
 	},

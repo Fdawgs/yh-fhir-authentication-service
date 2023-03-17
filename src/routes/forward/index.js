d2eeff45c4fa1dcabdeb15b87109e06c7aa87cf7
Frazer Smith <frazer.dev@outlook.com>
2023-03-17 09:34:32 +0000

    refactor(routes/forward): chain response functions

--- src/routes/forward/index.js
+++ src/routes/forward/index.js
@@ -89,15 +89,15 @@ async function route(server, options) {
 					 * Remove headers set by Mirth Connect that are either inaccurate
 					 * or pose security risks
 					 */
-					reply.removeHeader("access-control-allow-headers");
-					reply.removeHeader("access-control-allow-methods");
-					reply.removeHeader("access-control-expose-headers");
-					reply.removeHeader("etag");
-					reply.removeHeader("last-modified");
-					reply.removeHeader("location");
-					reply.removeHeader("server");
-
-					reply.send(targetResponse);
+					reply
+						.removeHeader("access-control-allow-headers")
+						.removeHeader("access-control-allow-methods")
+						.removeHeader("access-control-expose-headers")
+						.removeHeader("etag")
+						.removeHeader("last-modified")
+						.removeHeader("location")
+						.removeHeader("server")
+						.send(targetResponse);
 				},
 			});
 		},
